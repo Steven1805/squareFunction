3bd88b4f0e8cf9c4ab2360f9efd57df33af8dc86
Steven1805 <hei.steven.2@gmail.com>
2022-02-24 11:13:20 +0300

    My first Commit

---
+++ squareFunction.js
@@ -0,0 +1,18 @@
+//recursive square function 
+function SquareFunction(){
+    //the result equals to 0 if n=0
+    if (n==0) {
+        0
+    }
+    //a negative value of n will be calculated as |n|
+    else if (n<0){
+        (n^2)
+    }
+    //as the remarkable identity (n+1)^2 is equal to n^2 + 2n + 1, if we replace n by n-1, ((n-1)+1)^2=n^2 and the calcul will end to the result below 
+    else {
+        ((n-1)^2) + (2*n) - 1
+    }
+    return n*n
+}
+
+console.log(SquareFunction());
